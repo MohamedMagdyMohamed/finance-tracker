242be99b9863431752975a1dc98e6d1a3ea73ebd
Mohamed Magdy Mohamed <mm.medany96@gmail.com>
2019-11-24 14:25:50 +0200

    Add association user_stocks and fix flash display based on action

--- app/models/stock.rb
+++ app/models/stock.rb
@@ -1,5 +1,8 @@
 class Stock < ApplicationRecord
 
+  has_many :user_stocks
+  has_many :users, through: :user_stocks
+
   def self.new_from_lookup(ticker_symbol)
     StockQuote::Stock.new(api_key: 'pk_41825fda1b0b44508945e4e418d247d6')
     begin
